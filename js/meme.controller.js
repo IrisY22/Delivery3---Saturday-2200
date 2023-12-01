a2b3fbbeb8e7d3c3bf8998db873aa5ec35af681d
Iris Yatskaer <iriska1160@gmail.com>
2023-12-01 11:26:39 +0200

    Add renderMeme()

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,10 +1,25 @@
 "use strict";
+var gElCanvas;
+var gCtx;
 
 function onInit() {
+  gElCanvas = document.getElementById("meme-canvas");
+  _createImg();
   renderMeme();
 }
 
 function renderMeme() {
-  gElCanvas = document.querySelector("canvas");
-  // gCtx = gElCanvas.getContext("2d");
+  const imgData = getImg();
+  const memeData = getMeme();
+  gCtx = gElCanvas.getContext("2d");
+  gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
+
+  var img = new Image();
+  img.onload = function () {
+    gCtx.drawImage(img, 50, 60, this.width, this.height);
+  };
+  img.src = imgData.url;
+
+  gCtx.font = " 48px serif";
+  gCtx.fillText(memeData.lines[0].txt, 50, 40);
 }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,20 +1,47 @@
 "use strict";
-let gImgs;
-_createImgs();
+var gImgs;
+var gMeme;
+
+gMeme = _createMeme();
+
+function getMeme() {
+  return gMeme;
+}
+
+function getImg() {
+  return gImgs;
+}
+
+function setLineTxt(elDescriprion) {
+  console.log(elDescriprion.value);
+  gMeme.lines[0].txt = elDescriprion.value;
+  renderMeme();
+}
 
 // Private functions
 
-function _createImgs() {
-  gImgs = [
-    _createImg("img/1.jpg", ["funny", "trump"]),
-    _createImg("img/2.jpg", ["cute", "puppy"]),
-  ];
+// function _createImgs() {
+//   gImgs = [_createImg("img/2.jpg", ["cute", "puppy"])];
+// }
+
+function _createImg() {
+  gImgs = {
+    id: 1,
+    url: "img/2.jpg",
+    keywords: ["cute", "puppy"],
+  };
 }
 
-function _createImg(url, keywords) {
+function _createMeme(txt) {
   return {
-    id: makeId(),
-    url,
-    keywords,
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+      {
+        txt,
+        size: 20,
+        color: "red",
+      },
+    ],
   };
 }
